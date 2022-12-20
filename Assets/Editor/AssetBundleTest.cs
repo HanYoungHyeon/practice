676ab37ad7c2831a1c4727eacd800478e8e36946
HanYoungHyeon <112858988+HanYoungHyeon@users.noreply.github.com>
2022-12-20 18:37:33 +0900

    Fix : AssetBundleDownload
    에셋번들 다운로드방법 수정
    기존 로컬용 코드인 LoadFromFile과 서버 및 로컬용 LoadFromCacheOrDownload이 아닌 UnityWebRequest를 사용

--- Assets/Editor/AssetBundleTest.cs
+++ Assets/Editor/AssetBundleTest.cs
@@ -5,7 +5,7 @@ public class AssetBundleTest : MonoBehaviour
 	[MenuItem("Bundles/Bulid AssetBundles")]//에디터 네임스페이스를 사용하면 MenuItem을 통해 에디터 내 메뉴를 추가할 수 있다
 	static void BuildAllAssetBundles()//왜 static을 사용할까? 아마 에셋번들은 게임 시작부터 끝까지 필요하기 때문에 데이터영역에 할당하려고 쓰는 것 같다. 이 부분은 따로 찾아보자.
 	{
-		BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.Android);
+		BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.Android);
 		//BuildPipeline.BuildAssetBundles 메서드는 에셋번들로 구워주는 것. ("구워서 저장할폴더" , 압축하거나 그냥하거나 여러가지를 정해주는 옵션, 에셋번들을 구워서 어느 플랫폼에서 사용할 것인지 전용플랫폼 선택 )
 		//테스트결과 잘 만들어졌음.
 	}
--- Assets/Scripts/AssetBundleDownload.cs
+++ Assets/Scripts/AssetBundleDownload.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using System;
 
 public class AssetBundleDownload : MonoBehaviour
@@ -9,27 +10,34 @@ public class AssetBundleDownload : MonoBehaviour
 	//캐싱은 한번 다운로드받으면 두 번째부터는 이미 다운로드 받은 것을 불러오는 것.
 	//논캐싱은 매번 받는 것.
 	//유니티에서는 캐싱을 권함. 매번 받게되면 네트워크의 속도 저하가 발생할 수 있음.
-	
-	public int version; // 번들의 버전
+	public string bundleURL;
+	public string assetName;
+	public int version;
 
+    private void Awake()
+    {
+		assetName = "bundlecube";
+		bundleURL = "file:///C:/Users/shwuo/practice/Assets/AssetBundles/AssetBundles";
+	}
     void Start()
 	{
 		StartCoroutine(Download());
 	}
-
+	
 	IEnumerator Download()
 	{
 		// cache 폴더에 AssetBundle을 담을 것이므로 캐싱시스템이 준비 될 때까지 기다림
 		while (!Caching.ready)
 			yield return null;
-		// 에셋번들이 캐시에 있으면 로드하고, 없으면 다운로드하여 캐시폴더에 저장합니다.
-		using (WWW www = WWW.LoadFromCacheOrDownload("file://C:/Users/shwuo/practice/Assets/AssetBundles", version))
-		{
-			//WWW.LoadFromCacheOrDownload(에셋번들을 다운로드 받을 서버 주소. 서버는 없으니 로컬폴더경로를 사용할 예정, 번들버전)
-			yield return www;
-			if (www.error != null)
-				throw new Exception("WWW 다운로드에 에러가 생겼습니다.:" + www.error);
+		UnityWebRequest request = UnityWebRequest.Get(bundleURL);
+		//WWW.LoadFromCacheOrDownload(에셋번들을 다운로드 받을 서버 주소, 번들버전) 에셋번들이 캐시에 있으면 로드하고, 없으면 다운로드하여 캐시폴더에 저장
+			yield return request.Send();
 
-		} // using문은 File 및 Font 처럼 컴퓨터 에서 관리되는 리소스들을 쓰고 나서 쉽게 자원을 되돌려줄수 있도록 기능을 제공
-	}
+		AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+			
+				Instantiate(bundle.LoadAsset<GameObject>(assetName));
+
+			//bundle.Unload(false);
+		
+	}// 2022.12.20 현재 에셋번들을 빌드하고 다운로드하는 것까지는 성공했지만 로드하는 코드를 지원하지 않는 경우가 많아서 최신 방법을 다시 찾아보아야 함.
 }
